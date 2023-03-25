a4b309f5b18564c0f661c58edb6060c765ef4738
Benjamin Le Berre <benjamin@le-berre.com>
2023-03-25 19:26:01 +0100

    prettify test script

--- test.sh
+++ test.sh
@@ -1,24 +1,68 @@
 #!/bin/sh
 set -o errexit -o nounset
 
+ansi_green_fg="\e[32m"
+ansi_red_fg="\e[31m"
+ansi_color_reset="\e[0m"
+
 mkdir -p tests/
+
+# 1: input
+# 2: out_filename
+decode_reencode() {
+	zig-out/bin/8086_decoder < "$1" > tests/"$2.asm" && nasm --before 'bits 16' -o "tests/$2" "tests/$2.asm"
+}
+
+# 1: testfile
+ok() {
+	printf "${ansi_green_fg}O: %s${ansi_color_reset}\n" "$1"
+}
+
+# 1: testfile
+ko() {
+	printf "${ansi_red_fg}X: %s${ansi_color_reset}\n" "$1"
+}
 
 for file in testfiles/valid/* ; do
 	filename="${file#testfiles/valid}"
-	zig-out/bin/8086_decoder < "$file" > tests/"$filename.asm"
-	nasm --before 'bits 16' -o "tests/$filename" "tests/$filename.asm"
+
+	# Do not exit on error so that we go through all files
+	decode_reencode "$file" "$filename" || :
+
 	if cmp "$file" "tests/$filename" ; then
-		printf "%s: O\n" "$file"
+		ok "$file"
 	else
-		printf "%s: X\n" "$file"
+		ko "$file"
 	fi
 done
 
 for file in testfiles/invalid/* ; do
 	# TODO(benjamin): find error codes for invalid programs
 	if zig-out/bin/8086_decoder < "$file" > /dev/null 2>/dev/null; then
-		printf "%s: X\n" "$file"
+		ko "$file"
 	else
-		printf "%s: O\n" "$file"
+		ok "$file"
+	fi
+done
+
+# Assuming that names from testfiles/valid will not conflict since course files
+# are prefixed with "listing_".
+for file in course_material/perfaware/part1/listing_* ; do
+	case "$file" in
+		*.asm)
+			continue
+			;;
+		*)
+			;;
+	esac
+	filename="${file#course_material/perfaware/part1/}"
+
+	# Do not exit on error so that we go through all files
+	decode_reencode "$file" "$filename" || :
+
+	if cmp "$file" "tests/$filename" ; then
+		ok "$file"
+	else
+		ko "$file"
 	fi
 done
