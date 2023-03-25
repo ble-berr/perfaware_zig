44360c4d8464e96b82863daa89b57eba5ac8b548
Benjamin Le Berre <benjamin@le-berre.com>
2023-03-25 23:34:44 +0100

    add immediate to rm support for many ops

--- src/main.zig
+++ src/main.zig
@@ -522,6 +522,35 @@ fn decodeAccImmediate(
     }
 }
 
+fn decodeOpRmImmediate(width: OperandWidth, byte_stream: []const u8) !Instruction {
+    if (byte_stream.len < 2) {
+        return Error.IncompleteProgram;
+    }
+
+    const mod_byte = parseModByte(byte_stream[1]);
+    const mod_operand = try getModOperand(mod_byte, width, byte_stream[2..]);
+    const immediate = try getImmediateOperand(
+        width,
+        byte_stream[(2 + mod_operand.length)..],
+    );
+
+    return Instruction{
+        .length = 2 + mod_operand.length + immediate.length,
+        .type = switch (mod_byte.a) {
+            0 => .add,
+            1 => .@"or",
+            2 => .adc,
+            3 => .sbb,
+            4 => .@"and",
+            5 => .sub,
+            6 => .xor,
+            7 => .cmp,
+        },
+        .dst = mod_operand.operand,
+        .src = .{ .immediate = immediate.operand },
+    };
+}
+
 fn decodeInstruction(byte_stream: []const u8) !Instruction {
     if (byte_stream.len < 1) {
         return Error.IncompleteProgram;
@@ -583,6 +612,9 @@ fn decodeInstruction(byte_stream: []const u8) !Instruction {
         0x3b => decodeRegisterRM(.cmp, .from_rm, .word, byte_stream),
         0x3c => decodeAccImmediate(.cmp, .byte, byte_stream),
         0x3d => decodeAccImmediate(.cmp, .word, byte_stream),
+
+        0x80 => decodeOpRmImmediate(.byte, byte_stream),
+        0x81 => decodeOpRmImmediate(.word, byte_stream),
 
         0x88 => decodeRegisterRM(.mov, .to_rm, .byte, byte_stream),
         0x89 => decodeRegisterRM(.mov, .to_rm, .word, byte_stream),
