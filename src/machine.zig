6ecac504471a6654c949f377bee398a8c1ec5198
Benjamin Le Berre <benjamin@le-berre.com>
2023-04-01 21:27:57 +0200

    add machine flags

--- src/machine.zig
+++ src/machine.zig
@@ -7,6 +7,30 @@ pub const byte_registers = @ptrCast(*[8]u8, &word_registers);
 
 pub var segment_registers = [4]u16{ 0, 0, 0, 0 };
 
+pub const Flags = struct {
+    trap: bool,
+    direction: bool,
+    interrupt_enable: bool,
+    overflow: bool,
+    sign: bool,
+    zero: bool,
+    auxiliary_carry: bool,
+    parity: bool,
+    carry: bool,
+};
+
+pub var flags = Flags{
+    .trap = false,
+    .direction = false,
+    .interrupt_enable = false,
+    .overflow = false,
+    .sign = false,
+    .zero = false,
+    .auxiliary_carry = false,
+    .parity = false,
+    .carry = false,
+};
+
 pub fn reset() void {
     for (0..memory.len) |i| {
         memory[i] = 0;
@@ -21,4 +45,16 @@ pub fn reset() void {
     for (0..segment_registers.len) |i| {
         segment_registers[i] = 0;
     }
+
+    flags = Flags{
+        .trap = false,
+        .direction = false,
+        .interrupt_enable = false,
+        .overflow = false,
+        .sign = false,
+        .zero = false,
+        .auxiliary_carry = false,
+        .parity = false,
+        .carry = false,
+    };
 }
